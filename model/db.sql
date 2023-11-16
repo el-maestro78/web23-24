cd79cbbeaedac3c57d75c7d799fc78e025cc965d
CPP-Bot-78 <kzachoulitis@gmail.com>
2023-11-16 16:06:42 +0200

    most of the database ready

--- model/db.sql
+++ model/db.sql
@@ -1,55 +1,75 @@
 CREATE DATABASE webproject24;
 
+CREATE TYPE base_coordinates AS (
+    x DOUBLE PRECISION,
+    y DOUBLE PRECISION
+);
+
 CREATE TABLE civilian(
 civ_id SERIAL PRIMARY KEY,
 first_name VARCHAR(255) DEFAULT '',
 surname VARCHAR(255) DEFAULT '',
-username VARCHAR(255) DEFAULT '',
-pass VARCHAR(255) DEFAULT '',
-email VARCHAR(255) DEFAULT '',
+username VARCHAR(255) NOT NULL,
+pass VARCHAR(255) NOT NULL,
+email VARCHAR(255) DEFAULT ''
 );
 
 CREATE TABLE rescuer (
 resc_id SERIAL PRIMARY KEY,
 first_name VARCHAR(255) DEFAULT '',
 surname VARCHAR(255) DEFAULT '',
-username VARCHAR(255) DEFAULT '',
-pass VARCHAR(255) DEFAULT '',
-email VARCHAR(255) DEFAULT '',
+username VARCHAR(255) NOT NULL,
+pass VARCHAR(255) NOT NULL,
+email VARCHAR(255) DEFAULT ''
 );
 
 CREATE TABLE baseadmin (
 admin_id SERIAL PRIMARY KEY,
-base_loc ,
-username VARCHAR(255) DEFAULT '',
-pass VARCHAR(255) DEFAULT '',
-email VARCHAR(255) DEFAULT '',
+username VARCHAR(255) NOT NULL,
+pass VARCHAR(255) NOT NULL,
+email VARCHAR(255) DEFAULT ''
 );
 
 CREATE TABLE vehicles(
-
+veh_id SERIAL PRIMARY KEY,
+username VARCHAR(255) NOT NULL,
+location base_coordinates NOT NULL,
+resc_id INTEGER REFERENCES rescuer(resc_id)
 );
 
-CREATE TABLE warehouse(
-
+CREATE TABLE base(
+base_id SERIAL PRIMARY KEY,
+location base_coordinates NOT NULL,
 );
 
 CREATE TABLE items(
-
-);
-
-CREATE TABLE requests(
-
+item_id SERIAL PRIMARY KEY,
+subcategory INTEGER NOT NULL,
+details json,
 );
 
 CREATE TABLE news(
-
+news_id SERIAL PRIMARY KEY,
 );
 
 CREATE TABLE tasks(
+tasks_id SERIAL PRIMARY KEY,
+);
 
+CREATE TABLE requests(
+req_id SERIAL PRIMARY KEY,
+pending BOOLEAN DEFAULT FALSE,
+quantity INTEGER DEFAULT 1,
+reg_date DATE NOT NULL,
+assign_date DATE NOT NULL,
+civ_id INTEGER REFERENCES civilian(civ_id)
 );
 
 CREATE TABLE offers(
-
+off_id SERIAL PRIMARY KEY,
+pending BOOLEAN DEFAULT FALSE,
+quantity INTEGER DEFAULT 1,
+reg_date DATE NOT NULL,
+assign_date DATE NOT NULL,
+civ_id INTEGER REFERENCES civilian(civ_id)
 );
