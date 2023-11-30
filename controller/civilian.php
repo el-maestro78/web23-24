3fce9127c172a7cf8ab9a9fd49637a26a71b741b
Mike <michalissoko@gmail.com>
2023-11-30 18:11:02 +0200

    Civilian, some additional things.

--- controller/civilian.php
+++ controller/civilian.php
@@ -63,7 +63,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 //      3. Request Handling
 //TODO:
 $category;
+$NumOfCat;
 $personNum;
+$NumOfReq;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["category"])){
@@ -73,9 +75,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $category = test_input($_POST["category"]);
         $personNum = test_input($_POST["personNum"]);
+	$NumOfReq = $NumOfReq + 1;
      
     }   
 
+}
+
+if($NumOfCat>1)
+{
+echo "You can insert only one category per request.";
+} else {
+        $category = test_input($_POST["category"]);
+        $personNum = test_input($_POST["personNum"]);
+	$NumOfReq = $NumOfReq + 1;
+     
+    }   
+
+
+if($NumOfReq < 4)
+{
+echo "Please insert more requests.";
 }
 
 function test_input($data)
@@ -95,6 +114,54 @@ function test_input($data)
 
 //      4. Announcements & Offers
 //TODO:
+$NumOfOff;
+$category;
+$username;
+$password;
+$name;
+$surname;
+$phone;
+$email;
+$quantity;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["username"])){
+        echo "username is required";
+    } elseif (empty($_POST["password"])) {
+        echo "password is required";
+    } elseif (empty($_POST["name"])) {
+            echo "name is required";
+    } elseif (empty($_POST["surname"])) {
+            echo "surname is required"; 
+    } elseif (empty($_POST["phone"])) {
+            echo "phone is required"; 
+    } elseif (empty($_POST["email"])) {
+        echo "phone is required";
+    }  elseif (empty($_POST["category"])) {
+            echo "category is required";
+    } elseif (empty($_POST["quantity"])) {
+        echo "quantity is required";
+    }  
+    
+    }               
+    else {
+        $username = test_input($_POST["username"]);
+        $password = test_input($_POST["password"]);
+        $name = test_input($_POST["name"]);
+        $surname = test_input($_POST["surname"]);
+        $phone = test_input($_POST["phone"]);
+        $email = test_input($_POST["email"]);
+	$category = test_input($_POST["email"]);
+	$quantity = test_input($_POST["email"]);
+
+    }
+
+
+
+
+if($NumofOff < 3)
+{
+echo "Please insert more offers.";
+}
 
 
 ?>
