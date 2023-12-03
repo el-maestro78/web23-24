896d4c5230002b8063cee74e7aedd0beed16af6f
Theodore Giannak <59723723+TheodoreGiannak@users.noreply.github.com>
2023-12-03 14:25:27 +0200

    Small changes and comments again

--- controller/rescuer/rescuer_login.php
+++ controller/rescuer/rescuer_login.php
@@ -1,16 +1,18 @@
 <?php
+session_start();
 // 1. Login-Logout
 //same as admin's, no implementation needed here
-$typed_username;
-$typed_password;
+$typed_username = $_POST["username"];
+$typed_password = $_POST["password"];
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["typed_username"])){
+    //empty() can be replpaced from required in frontend
+    /*if (empty($typed_username)){ 
         echo "Username is required";
-    } elseif (empty($_POST["typed_password"])) {
+    } elseif (empty($typed_password)) {
         echo "Password is required";
     } else {
-        $username = validate_input($_POST["typed_username"]);
-        $password = validate_input($_POST["typed_password"]);
+        $username = validate_input($typed_username);
+        $password = validate_input($typed_password);
     }
     function validate_input($data)
     {
@@ -22,11 +24,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = <<<EOF
       SELECT username, pass 
       FROM dbUser 
-      WHERE username={$username} AND pass={$password};
+      WHERE username={$typed_username} AND pass={$typed_password};
     EOF;
     if(pg_num_rows($dbconn, $sql) > 0){
         echo "Logged in";
-    }
+    }*/
 }
 ?>
 
---
+++ model/db_additions.sql
@@ -0,0 +1,4 @@
+""" for login"""
+create extension pgcrypto
+
+INSERT INTO dbUser(username, pass, email)values('Test User', crypt('pass123', gen_salt('md5')), testemail@gmail.com)
