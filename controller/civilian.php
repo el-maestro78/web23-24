c1c88aafdd57ee38af125cd0781ea91a5db1c73f
Mike <michalissoko@gmail.com>
2023-12-22 11:49:14 +0200

    Civilian some changes.

--- controller/civilian.php
+++ controller/civilian.php
@@ -7,33 +7,33 @@ include("../../model/config.php");
 
 //      1. Account Creation
 //TODO:
-$username;
-$password;
-$name;
-$surname;
-$phone;
-$email;
+$Civilian_username;
+$Civilian_password;
+$Civilian_name;
+$Civilian_surname;
+$Civilian_phone;
+$Civilian_email;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["username"])){
+    if (empty($_POST["Civilian_username"])){
         echo "username is required";
-    } elseif (empty($_POST["password"])) {
+    } elseif (empty($_POST["Civilian_password"])) {
         echo "password is required";
-    } elseif (empty($_POST["name"])){
+    } elseif (empty($_POST["Civilian_name"])){
             echo "name is required";
-    } elseif (empty($_POST["surname"])) {
+    } elseif (empty($_POST["Civilian_surname"])) {
             echo "surname is required"; 
-    } elseif (empty($_POST["phone"]))
+    } elseif (empty($_POST["Civilian_phone"]))
             echo "phone is required"; 
-    } elseif (empty($_POST["email"]))
-        echo "phone is required";                 
+    } elseif (empty($_POST["Civilian_email"]))
+        echo "email is required";                 
     else {
-        $username = test_input($_POST["username"]);
-        $password = test_input($_POST["password"]);
-        $name = test_input($_POST["name"]);
-        $surname = test_input($_POST["surname"]);
-        $phone = test_input($_POST["phone"]);
-        $email = test_input($_POST["email"]);
+        $username = test_input($_POST["Civilian_username"]);
+        $password = test_input($_POST["Civilian_password"]);
+        $name = test_input($_POST["Civilian_name"]);
+        $surname = test_input($_POST["Civilian_surname"]);
+        $phone = test_input($_POST["Civilian_phone"]);
+        $email = test_input($_POST["Civilian_email"]);
 
     }
 
@@ -45,16 +45,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 //      2. Login-Logout
 //same as admin's, no implementation needed here
-$username;
-$password;
+$Civilian_username;
+$Civilian_password;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["username"])){
+    if (empty($_POST["Civilian_username"])){
         echo "Name is required";
-    } elseif (empty($_POST["password"])) {
+    } elseif (empty($_POST["Civilian_password"])) {
         echo "Password is required";
     } else {
-        $username = test_input($_POST["username"]);
-        $password = test_input($_POST["password"]);
+        $username = test_input($_POST["Civilian_username"]);
+        $password = test_input($_POST["Civilian_password"]);
 
     }  
 
@@ -64,21 +64,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 //TODO:
 $Req_id;
 $Off_id;
-$Off_category;
+$Civilian_Off_category;
 $Civ_id;
-$Req_category;
-$NumOfCat;
-$personNum;
+$Civilian_Req_category;
+$Civilian_NumOfCat;
+$Civilian_personNum;
 $NumOfReq;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["category"])){
+    if (empty($_POST["Req_category"])){
         echo "category is required";
     } elseif (empty($_POST["personNum"])) {
         echo "personNum is required";         
     } else {
-        $Req_category = test_input($_POST["category"]);
-        $personNum = test_input($_POST["personNum"]);
+        $category = test_input($_POST["Civilian_Req_category"]);
+        $personNum = test_input($_POST["Civilian_personNum"]);
 	$NumOfReq = $NumOfReq + 1;
      
     }   
@@ -89,8 +89,8 @@ if($NumOfCat>1)
 {
 echo "You can insert only one category per request.";
 } else {
-        $category = test_input($_POST["category"]);
-        $personNum = test_input($_POST["personNum"]);
+        $category = test_input($_POST["Civilian_Req_category"]);
+        $personNum = test_input($_POST["Civilian_personNum"]);
 	$NumOfReq = $NumOfReq + 1;
      
     }   
@@ -109,15 +109,15 @@ for($Civ_id = 0; $Civ_id < 3; $Civ_id++)
 for($Off_id=0; $Off_id<2; $Off_id++)
 {
 
-$Off_category = $Off_category;
-$Off_category = $Off_category + 1;
+$Civilian_Off_category = $Civilian_Off_category;
+$Civilian_Off_category = $Civilian_Off_category + 1;
 }
 
 
 for($Req_id = 0; $Req_id < 4; $Req_id++)
 {
  
-if($Reg_category != a AND $Reg_category != a+1)
+if($Civilian_Reg_category != a AND $Reg_category != a+1)
 {
 
 echo"Req_category doesn't match with any Off_categories";
@@ -125,8 +125,8 @@ echo"Req_category doesn't match with any Off_categories";
 
 else
 {
-$Req_category = test_input($_POST["category"]);
-$personNum = test_input($_POST["personNum"]);
+$category = test_input($_POST["Civilian_Req_category"]);
+$personNum = test_input($_POST["Civilian_personNum"]);
 $NumOfReq = $NumOfReq + 1;
 }
 
@@ -154,43 +154,43 @@ function test_input($data)
 $Civ_id;
 $Off_id;
 $NumOfOff;
-$Off_category;
-$username;
-$password;
-$name;
-$surname;
-$phone;
-$email;
-$Off_quantity;
+$Civilian_Off_category;
+$Civilian_username;
+$Civilian_password;
+$Civilian_name;
+$Civilian_surname;
+$Civilian_phone;
+$Civilian_email;
+$Civilian_Off_quantity;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["username"])){
+    if (empty($_POST["Civilian_username"])){
         echo "username is required";
-    } elseif (empty($_POST["password"])) {
+    } elseif (empty($_POST["Civilian_password"])) {
         echo "password is required";
-    } elseif (empty($_POST["name"])) {
+    } elseif (empty($_POST["Civilian_name"])) {
             echo "name is required";
-    } elseif (empty($_POST["surname"])) {
+    } elseif (empty($_POST["Civilian_surname"])) {
             echo "surname is required"; 
-    } elseif (empty($_POST["phone"])) {
+    } elseif (empty($_POST["Civilian_phone"])) {
             echo "phone is required"; 
-    } elseif (empty($_POST["email"])) {
-        echo "phone is required";
+    } elseif (empty($_POST["Civilian_email"])) {
+        echo "email is required";
     }  elseif (empty($_POST["Off_category"])) {
             echo "Off_category is required";
-    } elseif (empty($_POST["Off_quantity"])) {
+    } elseif (empty($_POST["Civilian_Off_quantity"])) {
         echo "Off_quantity is required";
     }  
     
     }               
     else {
-        $username = test_input($_POST["username"]);
-        $password = test_input($_POST["password"]);
-        $name = test_input($_POST["name"]);
-        $surname = test_input($_POST["surname"]);
-        $phone = test_input($_POST["phone"]);
-        $email = test_input($_POST["email"]);
-	$Off_category = test_input($_POST["email"]);
-	$Off_quantity = test_input($_POST["email"]);
+        $username = test_input($_POST["Civilian_username"]);
+        $password = test_input($_POST["Civilian_password"]);
+        $name = test_input($_POST["Civilian_name"]);
+        $surname = test_input($_POST["Civilian_surname"]);
+        $phone = test_input($_POST["Civilian_phone"]);
+        $email = test_input($_POST["Civilian_email"]);
+	$Off_category = test_input($_POST["Civilian_Off_category"]);
+	$Off_quantity = test_input($_POST["Civilian_Off_quantity"]);
 
     }
 
@@ -206,7 +206,4 @@ if($NumofOff < 3)
 
 }
 
-?>
-<?php
-include("../../model/dbclose.php");
 ?>
