3b40c9c03c0e82724c05317f074a3753e30b3dfc
Theodore Giannak <teognk@gmail.com>
2024-08-10 13:03:35 +0300

    Login UI (more to be added)

---
+++ views/home_page.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+     <title>Volunteers for Natular Disasters</title>
+</head>
+<body>
+    <?php
+    
+    ?>
+    <script>
+        
+    </script>
+    <h1> Home Page </h1>
+    <p> More to be added </P>
+<a href="login.php">
+    <button>Back to Login Page</button>
+</a>
+</body>
+</html>
--- views/login.php
+++ views/login.php
@@ -9,12 +9,58 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Login</title>
 </head>
+<?php
+    include("../model/config.php");
+//    $dbconn = getDbConnection();
+    include("../controller/login4all.php")
+?>  
+<style>
+<?php include 'CSS/main.css'; ?>
+</style>
 <body>
-    <?php 
-    
-    ?>
-    <script>
+    <div class="page">
+    <div class="container">
 
+        <div class="left_box">
+        <div class="login">Login</div>
+        <div class="box_text">You must be loggined in in order to access our page. This is important to avoid fake help requests and have an organised track of all requests.</div>
+        </div>
+
+        <div class="right_box">
+        <div class="form">
+            <label for="email">Email</label>
+            <input type="email" id="email">
+            <div class="gradient-line"></div>
+
+            <label for="password">Password</label>
+            <input type="password" id="password">
+            <div class="gradient-line"></div>
+
+            <input type="submit" id="submit" value="Submit">
+        </div>
+        </div>
+
+    </div>
+    </div>
+
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      const inputs = document.querySelectorAll('.form input');
+      const lines = document.querySelectorAll('.gradient-line');
+
+      inputs.forEach((input, index) => {
+        input.addEventListener('focus', () => {
+          lines[index].classList.add('active');
+        });
+
+        input.addEventListener('blur', () => {
+          if (!input.value.trim()) { // Check if the input is empty
+            lines[index].classList.remove('active');
+          }
+        });
+      });
+    });
     </script>
+
 </body>
 </html>
