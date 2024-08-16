ba2518be76115abb7a4a7f8ec9942252a314cd2a
El Maestro <kzachoulitis@gmail.com>
2024-08-16 14:05:54 +0300

    Update toolbar.php
    active page gets active css class

--- views/toolbar.php
+++ views/toolbar.php
@@ -28,22 +28,27 @@
 
     <body>
         <!-- Only for testing!! -->
-        <?php ($_SESSION['role']='admin') ?>
+        <?php include "index.php"?>
+        <?php //($_SESSION['role']='admin') ?>
+        <?php $current_page = $_SERVER['REQUEST_URI']; ?>
+
         <nav class="topnav">
-          <a class="active" href="<?php echo $base_url; ?>/views/home_page.php">Home</a>
-          <a href="<?php echo $base_url; ?>/views/maps/maps.php">Maps</a>
-          <a href="<?php echo $base_url; ?>/views/news/news.php">News</a>
-          <a href="<?php echo $base_url; ?>/404.php">Contact</a>
-          <a href="<?php echo $base_url; ?>/404.php">About</a>
-          <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') : ?>
-            <a href="<?php echo $base_url; ?>/views/storage/storage.php">Storage</a>
-            <a href="<?php echo $base_url; ?>/views/statistics/stats.php">Statistics</a>
-            <a href="<?php echo $base_url; ?>/views/addAccount/addAccount.php">Add Account</a>
-          <?php endif; ?>
-          <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'rescuer') : ?>
-            <a href="<?php echo $base_url; ?>/404.php">Add things here only for rescuer</a>
-            <a href="<?php echo $base_url; ?>/404.php">Add things here only for rescuer</a>
-          <?php endif; ?>
+            <a href="<?php echo $base_url; ?>/views/home_page.php" class="<?php echo strpos($current_page, 'home_page.php') !== false ? 'active' : ''; ?>">Home</a>
+            <a href="<?php echo $base_url; ?>/views/maps/maps.php" class="<?php echo strpos($current_page, 'maps.php') !== false ? 'active' : ''; ?>">Maps</a>
+            <a href="<?php echo $base_url; ?>/views/news/news.php" class="<?php echo strpos($current_page, 'news.php') !== false ? 'active' : ''; ?>">News</a>
+            <a href="<?php echo $base_url; ?>/404.php" class="<?php echo strpos($current_page, 'contact.php') !== false ? 'active' : ''; ?>">Contact</a>
+            <a href="<?php echo $base_url; ?>/404.php" class="<?php echo strpos($current_page, 'about.php') !== false ? 'active' : ''; ?>">About</a>
+
+            <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') : ?>
+                <a href="<?php echo $base_url; ?>/views/storage/storage.php" class="<?php echo strpos($current_page, 'storage.php') !== false ? 'active' : ''; ?>">Storage</a>
+                <a href="<?php echo $base_url; ?>/views/statistics/stats.php" class="<?php echo strpos($current_page, 'stats.php') !== false ? 'active' : ''; ?>">Statistics</a>
+                <a href="<?php echo $base_url; ?>/views/addAccount/addAccount.php" class="<?php echo strpos($current_page, 'addAccount.php') !== false ? 'active' : ''; ?>">Add Account</a>
+            <?php endif; ?>
+
+            <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'rescuer') : ?>
+                <a href="<?php echo $base_url; ?>/404.php" class="<?php echo strpos($current_page, 'rescuer_link1.php') !== false ? 'active' : ''; ?>">Add things here only for rescuer</a>
+                <a href="<?php echo $base_url; ?>/404.php" class="<?php echo strpos($current_page, 'rescuer_link2.php') !== false ? 'active' : ''; ?>">Add things here only for rescuer</a>
+            <?php endif; ?>
         </nav>
 
 
