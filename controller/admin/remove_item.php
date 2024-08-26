21e1bfce794dee081d17668b652cb52ebffff49b
El Maestro <kzachoulitis@gmail.com>
2024-08-27 00:31:39 +0300

    remove item works

--- controller/admin/remove_item.php
+++ controller/admin/remove_item.php
@@ -1,22 +1,24 @@
 <?php
-function remove_item($item_name){
-    $sql = <<< EOF
-            SELECT iname
-            FROM items
-            WHERE iname = $item_name;
-        EOF;
-    $check_if_already_exists = pg_query($dbconn, $sql);
+include "../../model/config.php";
+include "../../ini.php";
+include "../../auxiliary.php";
 
-    if ($check_if_already_exists && pg_num_rows($check_if_already_exists) <= 0) {
-        echo "Item $item_name doesn't exist";
-    } else {
+$item_name = validate_input($_POST['item']);
 
-        $sql = "DELETE FROM items WHERE iname = $item_name";
-        $result = pg_query($dbconn, $sql);
-
-        if (!$result) {
-            $error_message = "Problem with deleting item $item_name";
-            echo $error_message;
-        }
+$sql = <<< EOF
+        SELECT iname
+        FROM items
+        WHERE item_id = $1;
+       EOF;
+$check_if_already_exists = pg_query_params($dbconn, $sql, array($item_name));
+if ($check_if_already_exists && pg_num_rows($check_if_already_exists) <= 0) {
+    echo json_encode(['exists' => false, 'removed'=> false, 'error' => pg_last_error($dbconn)]);
+} else {
+    $sql = "DELETE FROM items WHERE item_id = $1";
+    $result = pg_query_params($dbconn, $sql, array($item_name));
+    if (!$result) {
+        echo json_encode(['exists' => true, 'removed'=> false, 'error' => pg_last_error($dbconn)]);
+    }else{
+        echo json_encode(['exists' => true, 'removed'=> true]);
     }
 }
--- views/storeManage/remove_item_form.php
+++ views/storeManage/remove_item_form.php
@@ -9,17 +9,65 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="icon" href="../favico/favicon.ico" type="image/x-icon">
-    <title>Sign Up</title>
+    <title>Remove Item</title>
 </head>
 <body>
     <?php
         include '../../ini.php';
         include '../../check_login.php';
         include '../toolbar.php';
-        echo "Hi";
+        include '../../controller/admin/fetch_items.php';
     ?>
+    <div class="container">
+            <div class="form_box">
+                <div class="details">Remove Item</div>
+                <div class="form">
+                    <label for="item" class="insert_label">Select Item to Remove</label>
+                    <select id="item" class="select_input" required>
+                        <?php
+                            foreach ($items_array as $item) {
+                                $id = $item['item_id'];
+                                $name = $item['iname'];
+                                $quant = $item['quantity'];
+                                echo "<option value=\"$id\">$name - $quant</option>";
+                            }
+                        ?>
+                    </select>
+                    <input type="submit" class="button_input" id="submit" value="Submit">
+                </div>
+            </div>
+    </div>
     <script>
+        const itemInput = document.getElementById('item');
+        const submit = document.getElementById('submit');
 
+        submit.addEventListener('click', function (event){
+            event.preventDefault()
+            submit_data();
+        });
+
+        function submit_data(){
+            const item = itemInput.value;
+            const params = new URLSearchParams();
+            params.append('item', item);
+
+            fetch('../../controller/admin/remove_item.php', {
+                method:'POST',
+                body:params
+            })
+            .then(response => response.json())
+            .then(data =>{
+                if (data.removed) {
+                        alert('Removed successfully');
+                        window.location.href ='storeManage.php';
+                    } else if(!data.removed && !data.exists){
+                        alert('Item doesn\'t exist');
+                    }else{
+                    alert('Error: Item is needed or loaded in a vehicle');// + data.error);
+                }
+            })
+            .catch(error => console.error('Error removing this item from the database:', error));
+        }
     </script>
 </body>
 </html>
