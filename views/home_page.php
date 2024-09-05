8ce0d00dbd0849582d7eb9d6968415a51967b6c0
El Maestro <kzachoulitis@gmail.com>
2024-09-05 16:46:37 +0300

    Update home_page.php

--- views/home_page.php
+++ views/home_page.php
@@ -8,6 +8,29 @@
         <link rel="icon" href="./favico/favicon.ico" type="image/x-icon">
         <title>Volunteers for Natural Disasters</title>
     </head>
+    <style>
+        .out-container {
+            background-color: #790e0e;
+            display: flex;
+            justify-content: center;
+            min-height: 100vh;
+        }
+        .in-container {
+            background-color: #f8f9fa;
+            padding: 20px;
+            border-radius: 10px;
+            text-align: center;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 20px;
+
+            width: 50%;
+        }
+        img {
+            max-width: 200px;
+            height: auto;
+            margin-top: 2px;
+        }
+    </style>
     <body>
         <?php
         include '../ini.php';
@@ -17,10 +40,20 @@
         <script>
 
         </script>
-        <h2> Home Page </h2>
-        <p> More to be added </p>
-        <a href="./login/login.php">
-            <button>Back to Login Page</button>
-        </a>
+        <div class="out-container">
+            <div class="in-container">
+                <img src="favico/rescue_logo.png" alt="Volunteer's Page Logo">
+                <h2> Volunteers for Natural Disasters </h2>
+                <p> Welcome to our platform, <?= $_SESSION['first_name']?>!
+                    <?php if($_SESSION['role']=='rescuer') :?>
+                        Thanks for your service. Look in the map for active tasks
+                    <?php elseif ($_SESSION['role'] == 'civilian') : ?>
+                        You can navigate through the bar on the top. For Requests go to...
+                    <?php else : ?>
+                        You 're an admin
+                    <?php endif; ?>
+                </p>
+            </div>
+        </div>
     </body>
 </html>
