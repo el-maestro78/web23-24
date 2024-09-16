0a6f24d61ec3acdd306483442d589fcff85c3aca
El Maestro <kzachoulitis@gmail.com>
2024-09-16 12:06:37 +0300

    added min distance
    store and vehicle popup dont get same coords

--- controller/admin/add_vehicles.php
+++ controller/admin/add_vehicles.php
@@ -11,27 +11,30 @@ $bases_obj = array();
 foreach ($bases as $base) {
     $bases_obj[] = new Base($base['base_id'], $base['lat'], $base['long']);
 }
-function generateRandomCoordinates($lat, $lng, $radius)
-{
-    $earth_radius = 6371;
-    $radius_in_degrees = $radius / $earth_radius;
-    $random_distance = $radius_in_degrees * sqrt(mt_rand() / mt_getrandmax());
-    $random_angle = mt_rand(10, 360) * (M_PI / 180); // Random angle in radians
+function generateRandomCoordinates($lat, $lng, $min_radius, $max_radius): array
+{ //$sea_lat=38.250114, $sea_long=21.735844
+    $earth_radius = 6371; // Earth's radius in kilometers
+    $max_radius_in_degrees = $max_radius / $earth_radius;
+    $min_radius_in_degrees = $min_radius / $earth_radius;
+    do {
+        $random_distance = $max_radius_in_degrees * sqrt(mt_rand() / mt_getrandmax());
+    } while ($random_distance < $min_radius_in_degrees); // Ensure it is not too close
+
+    $random_angle = mt_rand(0, 360) * (M_PI / 180); // Random angle in radians
 
     $new_lat = $lat + ($random_distance * cos($random_angle));
     $new_lng = $lng + ($random_distance * sin($random_angle)) / cos(deg2rad($lat));
-
+    
     return array('lat' => $new_lat, 'long' => $new_lng);
 }
 
 // Create vehicles at random positions and print results
 $vehicles = array();
+$min_distance = 1;
+$max_distance = 5;
 foreach ($bases_obj as $base) {
-    $vehicle_position = generateRandomCoordinates($base->lat, $base->long, 5); // 5km radius
+    $vehicle_position = generateRandomCoordinates($base->lat, $base->long, 1, 5); // 5km radius
     $vehicles[] = $vehicle_position;
-    //DEBUG
-    //echo "Vehicle Lat: " . $vehicle_position['lat'] . ", Vehicle Long: " . $vehicle_position['long'] . "<br>\n";
-    //echo "Base ID: " . $base->base_id . ", Base Lat: " . $base->lat . ", Base Long: " . $base->long . "<br>\n";
 }
 
 include("./update_vehicles.php");
